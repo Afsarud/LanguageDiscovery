61b2814370692c71eb14dd20cf12ecc5220d1192
AFSAR\User <engafsar5577@gmail.com>
2025-08-17 21:43:05 +0600

    Palaygo

--- Language.Discovery/AutoEmail/obj/Debug/netcoreapp3.1/AutoEmail.AssemblyInfo.cs
+++ Language.Discovery/AutoEmail/obj/Debug/netcoreapp3.1/AutoEmail.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoEmail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c217a7870bafcfbcbe44424c9c7e4b76693bcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoEmail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoEmail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
